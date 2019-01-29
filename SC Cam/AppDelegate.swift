f0bb7494b9146222dfd91c0a0efa0e58d58c1ef4
Jerry Volcy <jvolcy@jerrys-macbook-pro.local>
2019-01-29 13:37:15 -0500

    Initial Commit

---
+++ SC Cam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SC Cam
+//
+//  Created by Jerry Volcy on 1/29/19.
+//  Copyright © 2019 Jerry Volcy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SC Cam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SC Cam
+//
+//  Created by Jerry Volcy on 1/29/19.
+//  Copyright © 2019 Jerry Volcy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
