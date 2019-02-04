3ec8087ab868e591009fbec4fdfea3a1409352f8
Jerry Volcy <jvolcy@jerrys-macbook-pro.local>
2019-02-03 20:53:28 -0500

    image fie dialog completed.

--- SC Cam/AppDelegate.swift
+++ SC Cam/AppDelegate.swift
@@ -8,38 +8,52 @@
 
 import Cocoa
 
+//---------- Globals ----------
+//global pointer to the view controller
 var viewControllerGp: ViewController?
-
-//global pointer to app menu
-var appMenuGp: NSMenu?
 
 //global pointer to app delegate
 var appDelegateGp: AppDelegate?
 
+
+/* ============================================================================
+ ============================================================================ */
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    //----------  ----------
     @IBOutlet weak var appMenu: NSMenu!
     
     override init() {
         super.init()
         //connect the global app delegate pointer to the app delegate
         appDelegateGp = self
+        
     }
     
+    /* ============================================================================
+     ============================================================================ */
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        //viewControllerGp = view
     }
 
+    /* ============================================================================
+     ============================================================================ */
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
     
-    //function to quit when the user clicks the close button
+    /* ============================================================================
+     function to quit when the user clicks the close button.
+     Returning true causes the app to quit when the user clicks the close button.
+     ============================================================================ */
     func applicationShouldTerminateAfterLastWindowClosed(_ app: NSApplication) -> Bool {
         return true
     }
 
+    /* ============================================================================
+     ============================================================================ */
     @IBAction func menuSelect(_ sender: Any) {
             if let gp = viewControllerGp {
                 gp.menuBackgroundSelect(sender)
@@ -47,7 +61,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
     }
     
-    
+    /* ============================================================================
+     ============================================================================ */
+    @IBAction func menuDeviceSelect(_ sender: Any) {
+        if let gp = viewControllerGp {
+            gp.menuDeviceSelect(sender)
+        }
+        
+    }
+
     
 }
 
--- SC Cam/ViewController.swift
+++ SC Cam/ViewController.swift
@@ -9,78 +9,34 @@
 import Cocoa
 import AVFoundation
 
+/* ============================================================================
+============================================================================ */
 class ViewController: NSViewController {
     
+    //----------  ----------
     @IBOutlet var camView: NSView!
     @IBOutlet weak var imgView: NSImageView!
     
+    //----------  ----------
     let captureSession = AVCaptureSession()
     var captureDevice : AVCaptureDevice?
     var previewLayer : AVCaptureVideoPreviewLayer?
     var cameras : [AVCaptureDevice] = []
     
     
+    /* ============================================================================
+     ============================================================================ */
     override func viewDidLoad() {
         super.viewDidLoad()
         viewControllerGp = self
-        
-
-        if let appDeg = appDelegateGp {
-            //connect the global menu pointer to the app's menu
-            appMenuGp = appDeg.appMenu
-            
-        }
-
-    }
-        
-    func checkMenuItem(menuItem : NSMenuItem) {
-        //add a check mark by the selected item
-        menuItem.state = .on
-    }
-    
-    func uncheckMenuItem(menuItem : NSMenuItem) {
-        //remove a check mark by the selected item
-        menuItem.state = .off
-    }
-
-    func unCheckAllCaptureMenuDevices() {
-        //remove a check mark by all items in the "Capture Device" menu
-        if let menuGp = appMenuGp {
-            for menuItem in menuGp.items {
-                uncheckMenuItem(menuItem: menuItem)
-            }
-        }
-    }
 
 
-    func captureDeviceDeviceMenuEnableOption(deviceNumber: Int, enable: Bool, deviceName: String?){
-        let mnuItemDevice = appMenuGp?.item(withTitle: "Capture Device")
-        let y = mnuItemDevice?.submenu?.item(at: deviceNumber)
-        y?.title = deviceName ?? "?"
-        y?.isHidden = !enable
-    }
-    
-    override var representedObject: Any? {
-        didSet {
-            // Update the view, if already loaded.
-        }
-    }
-    
-    @IBAction func btnStartAction(_ sender: Any) {
-
-        let imgName = "file:///Users/jvolcy/Desktop/1280x720.png"
-        
-        let url = URL(string: imgName)
-
-        imgView.image = NSImage(byReferencing: url!)
-
-        //=======================================================================================
-        
+        //----------  ----------
         camView.layer = CALayer()
         camView.layer?.frame = CGRect(x:880, y:0, width:400, height:720)
         //camView.layer?.frame = self.view.frame
-        
-        // Do any additional setup after loading the view, typically from a nib.
+        //camView.layer?.contentsGravity = AVLayerVideoGravity.resizeAspectFill.rawValue
+
         captureSession.sessionPreset = AVCaptureSession.Preset.low
         
         // Get all audio and video devices on this machine
@@ -90,40 +46,111 @@ class ViewController: NSViewController {
         for device in devices {
             print(device)
             
-            // Camera object found and assign it to captureDevice
+            // Camera object found: add it to the cameras list
             if device.hasMediaType(AVMediaType.video) {
-                print(device)
                 cameras.append(device)
+            }
                 
-                captureDevice = device //as AVCaptureDevice
-                
-                //let x = devices.firstIndex(of: device)
-                
-                captureDeviceDeviceMenuEnableOption(deviceNumber: devices.firstIndex( of: device)!, enable: true, deviceName: captureDevice?.localizedName)
+            for i in 0..<cameras.count {
+                captureDeviceMenuEnableOption(deviceNumber: i, enable: true, deviceName: cameras[i].localizedName)
+            }
 
+        }
+ 
+
+    }
+        
+    /* ============================================================================
+     ============================================================================ */
+    func checkMenuItem(menuItem : NSMenuItem) {
+        //add a check mark by the selected item
+        menuItem.state = .on
+    }
+    
+    /* ============================================================================
+     ============================================================================ */
+    func uncheckMenuItem(menuItem : NSMenuItem) {
+        //remove a check mark by the selected item
+        menuItem.state = .off
+    }
+
+    /* ============================================================================
+     ============================================================================ */
+    func unCheckAllCaptureMenuDevices() {
+        //remove a check mark by all items in the "Capture Device" menu
+        //start by getting a reference to the capture device sub-menu
+        if let devMenu = appDelegateGp?.appMenu.item(withTitle: "Capture Device")?.submenu?.items {
+            
+            //now go through an uncheck every entry in the capture device submenu
+            for menuItem in devMenu {
+                uncheckMenuItem(menuItem: menuItem)
+                print("unchecking ", menuItem.title)
             }
         }
+    }
+
+
+    /* ============================================================================
+     ============================================================================ */
+    func captureDeviceMenuEnableOption(deviceNumber: Int, enable: Bool, deviceName: String?){
+        let mnuItemDevice = appDelegateGp?.appMenu.item(withTitle: "Capture Device")
+        let y = mnuItemDevice?.submenu?.item(at: deviceNumber)
+        y?.title = deviceName ?? "?"
+        y?.isHidden = !enable
+    }
+    
+    /* ============================================================================
+     ============================================================================ */
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    /* ============================================================================
+     ============================================================================ */
+    @IBAction func btnStartAction(_ sender: Any) {
+
+        let imgName = "file:///Users/jvolcy/Desktop/880x720.png"
         
+        let url = URL(string: imgName)
+
+        imgView.image = NSImage(byReferencing: url!)
+
+    }
+    
+    /* ============================================================================
+     ============================================================================ */
+    func startCaptureSession(captureDevice: AVCaptureDevice?) {
         
+        //print("capture device = ", captureDevice?.localizedName)
+        
+        //---------- halt any active session ----------
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+        }
+
+        //---------- remove all capture inputs ----------
+        for input in captureSession.inputs {
+            captureSession.removeInput(input)
+        }
+
         if captureDevice != nil {
             
             do {
                 
-                try captureSession.addInput(AVCaptureDeviceInput(device: (captureDevice ?? nil)!))
+                try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice!))
                 previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-                //previewLayer?.frame = (self.camView.layer?.frame)!
+ 
                 previewLayer?.frame = CGRect(x:0, y:0, width:400, height:720)
                 
                 previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
                 //previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspect
                 
-                //camView.layer?.contentsGravity = AVLayerVideoGravity.resizeAspectFill.rawValue
-                
                 
                 // Add previewLayer into custom view
                 self.camView.layer?.addSublayer(previewLayer!)
-                
-                
+
                 // Start camera
                 captureSession.startRunning()
                 
@@ -131,14 +158,60 @@ class ViewController: NSViewController {
                 print(AVCaptureSessionErrorKey.description)
             }
         }
+
     }
-    
-    
+
+    /* ============================================================================
+     ============================================================================ */
     func menuBackgroundSelect(_ sender: Any) {
+        /*
         if let menuItem = sender as? NSMenuItem{
             print(menuItem.title)
         }
+ */
+
+        let myFileDialog = NSOpenPanel()
+        myFileDialog.runModal()
+        
+        if let url = myFileDialog.url {
+            imgView.image = NSImage(byReferencing: url)
+        }
     }
 
+    /* ============================================================================
+     ============================================================================ */
+    func menuDeviceSelect(_ sender: Any) {
+        if let selectedDeviceMenuItem = sender as? NSMenuItem{
+            print("Selected Device:", selectedDeviceMenuItem.title)
+            
+            //---------- search the camera names to find the selected camera ----------
+            for i in 0..<cameras.count {
+                if cameras[i].localizedName == selectedDeviceMenuItem.title {
+                    //---------- uncheck all cameras in the device menu ----------
+                    unCheckAllCaptureMenuDevices()
+                    //---------- check the selected camera in the device menu ----------
+                    checkMenuItem(menuItem: selectedDeviceMenuItem)
+                    //---------- start the selected camera and break ----------
+                    startCaptureSession(captureDevice: cameras[i])
+                    break;
+                }   //if
+            }   //for
+            
+        }   //if let
+    }   //func
 
-    }
+}   //class
+
+
+
+
+
+/* ============================================================================
+ Scratch
+ ============================================================================ */
+//----------  ----------
+//----------  ----------
+//----------  ----------
+
+/* ============================================================================
+ ============================================================================ */
